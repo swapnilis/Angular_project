8794656d1f255e3b138f58edef3cba24bb754144
Swapnil <swapnilsingh315@gmail.com>
2023-07-17 20:49:24 +0530

    committing after adding angular gauge and few angular material

--- video-game-database/src/app/app.module.ts
+++ video-game-database/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+import {GaugeModule} from 'angular-gauge';
+import {MatTabsModule} from '@angular/material/tabs'
+import {MatIconModule} from '@angular/material/icon'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select'
 
 @NgModule({
   declarations: [
@@ -11,6 +19,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,HttpClientModule,
+    GaugeModule.forRoot(),
+    MatTabsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
